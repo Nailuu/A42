bb28ad4096b9edc8834c1983d4bec35c866aef52
Norias1011 <anthonykinzelin@gmail.com>
2025-08-02 20:27:59 +0200

    change in the server action

--- server/actions/sendMessageEmailJs.ts
+++ server/actions/sendMessageEmailJs.ts
@@ -19,8 +19,9 @@ const sendEmailJS = async (formData: FormData): Promise<EmailJSResponse> => {
     const serviceId = process.env.EMAILJS_SERVICE_ID;
     const templateId = process.env.EMAILJS_TEMPLATE_ID;
     const publicKey = process.env.EMAILJS_PUBLIC_KEY;
+    const privateKey = process.env.EMAILJS_PRIVATE_KEY;
 
-    if (!serviceId || !templateId || !publicKey) {
+    if (!serviceId || !templateId || !publicKey || !privateKey) {
       console.error("EmailJS credentials are missing");
       return { success: false, message: "Email service configuration error." };
     }
@@ -34,6 +35,7 @@ const sendEmailJS = async (formData: FormData): Promise<EmailJSResponse> => {
         service_id: serviceId,
         template_id: templateId,
         user_id: publicKey,
+        accessToken: privateKey,
         template_params: {
           firstname: formData.firstName,
           lastname: formData.lastName,
